0634cdb9f36e324e86f8c63c4c13d82ca8a434ce
antoniocrorusso <56083560+antoniocrorusso@users.noreply.github.com>
2020-09-20 15:46:59 -0300

    movimento e seguir

---
+++ UNIDOSjam/Assets/TestStuff/Script/FollowOnTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowOnTrigger : MonoBehaviour
+{
+    public float vel;
+
+    public float distance;
+    
+    private Transform target;
+
+    //private Animator boxAnimator;
+    
+    private Vector2 boxFollow;
+
+    public GameObject player;
+
+    public bool isTouched;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        //boxAnimator = FindObjectOfType<Animator>();
+        
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == target)
+        {
+            isTouched = true;
+        }
+
+    }
+
+
+    void Update()
+    {
+
+        if (isTouched) {
+
+            boxFollow.x = Input.GetAxisRaw("Horizontal");
+            boxFollow.y = Input.GetAxisRaw("Vertical");
+
+            if (Vector2.Distance(transform.position, target.position) > distance) {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, vel * Time.deltaTime);
+
+                //boxAnimator.SetInteger("Ghost_Horizontal", (int)ghostMovement.x);
+                //boxAnimator.SetInteger("Ghost_Vertical", (int)ghostMovement.y);
+            }
+
+        }
+    }
+}
---
+++ UNIDOSjam/Assets/TestStuff/Script/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 0.0f;
+    public float moveX = 0.0f;
+    public float moveY = 0.0f;
+
+
+    void Start()
+    {
+      
+    }
+
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+
+        gameObject.transform.position = new Vector2(transform.position.x + (moveX * speed * Time.deltaTime),
+            transform.position.y + (moveY * speed * Time.deltaTime));
+    }
+    
+   
+}
