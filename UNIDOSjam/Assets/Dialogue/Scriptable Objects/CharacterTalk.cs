e3fc1556feedcae645252b98b761e71e08467818
jaquesoler <jaquesoler@gmail.com>
2020-09-29 15:20:56 +0100

    Canvas added

---
+++ UNIDOSjam/Assets/Dialogue/Scriptable Objects/CharacterTalk.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+public class CharacterTalk : ScriptableObject
+{
+    public string fullName;
+    public Sprite portrait;
+}
