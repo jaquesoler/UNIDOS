e2cf008e8a888454cd97aee0a44deb5476a0cc44
jaquesoler <jaquesoler@gmail.com>
2020-10-01 19:47:38 +0100

    Musica mantem atraves das cenas.

---
+++ UNIDOSjam/Assets/Script/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance = null;
+    public static AudioManager Instance{
+        get{return instance;}
+    }
+
+    void Start(){
+        if(instance != null && instance != this){
+            Destroy(this.gameObject);
+            return;
+        } else {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
