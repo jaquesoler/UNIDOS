cce2a0e68551be4f1138097c0489de9517e93e9b
jaquesoler <jaquesoler@gmail.com>
2020-10-03 19:39:58 +0100

    Cena final, arrumações no jogo, polimento, melhorias visuais, game loop

---
+++ UNIDOSjam/Assets/Script/WaitBeforeAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitBeforeAnimation : MonoBehaviour
+{
+    public GameObject container;
+    public GameObject livro;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(UrucureaCoroutine());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator UrucureaCoroutine(){
+        Debug.Log("Started Coroutineaaa at timestamp : " + Time.time);
+        yield return new WaitForSeconds(15);                
+        container.SetActive(false);
+        livro.SetActive(true);
+        Camera.main.GetComponent<Animator>().enabled = true;
+    }
+}
