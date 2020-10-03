96dd27c72a9627409177c0eec2b31115e5ac3a92
jaquesoler <jaquesoler@gmail.com>
2020-10-03 23:54:09 +0100

    lofi e implementação do fim

--- UNIDOSjam/Assets/Script/WaitBeforeAnimation.cs
+++ UNIDOSjam/Assets/Script/WaitBeforeAnimation.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WaitBeforeAnimation : MonoBehaviour
 {
     public GameObject container;
     public GameObject livro;
 
+    public GameObject fadeOut;
+
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(UrucureaCoroutine());
-
+        StartCoroutine(TransitCoroutine());
     }
 
     // Update is called once per frame
@@ -27,5 +30,19 @@ public class WaitBeforeAnimation : MonoBehaviour
         container.SetActive(false);
         livro.SetActive(true);
         Camera.main.GetComponent<Animator>().enabled = true;
+    }
+
+    IEnumerator TransitCoroutine(){
+        Debug.Log("Started Coroutineaaa at timestamp : " + Time.time);
+        yield return new WaitForSeconds(22);                
+        fadeOut.GetComponent<FadeInFadeOut>().fade();
+
+        StartCoroutine(ChangeSceneCoroutine());
+    }
+
+    IEnumerator ChangeSceneCoroutine(){
+        
+        yield return new WaitForSeconds(8);                
+        SceneManager.LoadScene("Final");
     }
 }
